1ddae8633246dba21180752a3c1d3365ff6dfac4
erikmartel <erikdmartel@gmail.com>
2023-06-19 15:12:04 -0400

    About us component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 
-import './App.css';
+//*import './App.css';*//
 import Navigation from './Navigation/Nav';
-
+import Hero from './Hero/Hero'
 function App() {
 
   return (
     <div className="App">
       <Navigation />
+      <section className="hero-section">
+      <Hero />
+      </section>
     </div>
   );
 }
