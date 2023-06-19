30ff93a82f777e4b496fc6ca2e4d2c5145f2e9c5
erikmartel <erikdmartel@gmail.com>
2023-06-19 14:58:35 -0400

    Add Hero component

---
+++ src/Hero/Hero.js
@@ -0,0 +1,19 @@
+import './Hero.css';
+
+
+const Hero = () => {
+
+
+    return (
+        <div class="hero-image">
+            <div class="hero-text">
+                <h1>Find Your Park</h1>
+                <p>And I'm a Photographer</p>
+                <button>Hire me</button>
+                <button>Hire me</button>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
