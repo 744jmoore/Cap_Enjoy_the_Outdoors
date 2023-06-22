ab252f4b1010a62500646e30107aa9d83e5e35dc
erikmartel <erikdmartel@gmail.com>
2023-06-22 15:05:37 -0400

    Added star ratings & created topRatedData

--- src/Rating/Rating.js
+++ src/Rating/Rating.js
@@ -1,14 +1,47 @@
 import "./Rating.css";
 import StarRating from "../StarRating/StarRating";
+import Toprated from "../Toprated/Toprated";
 
 const Rating = () => {
+
+  const topRatedData = [
+    {
+      img: 'public/pics/EOsceola-StoryImg_2.jpg',
+      parkName: 'EOsceola Story',
+      review: 'Great time! We cannot wait to return!',
+      rating: 4,
+    },
+    {
+      img: 'public/pics/Cannon-StoryImg_2.jpg',
+      parkName: 'Cannon',
+      review: 'Fantastic Views! - Well worth the hike!',
+      rating: 5,
+    },
+    {
+      img: 'public/pics/Hale-StoryImg.jpg',
+      parkName: 'Hale Story',
+      review: 'Finished all 48 - Glad I saved this one for last!!',
+      rating: 4,
+    },
+    {
+     img: 'public/pics/OwlsHead-StoryImg.jpg',
+      parkName: 'Owls Head',
+      review: 'Great walking trails and interesting historical information on this one!',
+      rating: 5,
+    },
+  ];
+
   return (
-    <div className="Rating">
-      <img alt="placeholder"></img>
-      <h3>Park Name</h3>
-      <p>Review description</p>
-      <StarRating rating={5}/>
+    <>
+    {topRatedData.map(rating => {
+    <div key={topRatedData.img} className="Rating">  
+                <img src={topRatedData.img} />
+                <h3>{topRatedData.parkName}</h3>
+                <p>{topRatedData.review}</p>
+                <StarRating rating={topRatedData.rating}/>
+              
     </div>
+  })}</>
   );
 }
 
--- src/Toprated/Toprated.js
+++ src/Toprated/Toprated.js
@@ -2,16 +2,18 @@ import './Toprated.css';
 import Rating from '../Rating/Rating';
 
 
+
+
 const Toprated = () => {
+
+
     return (  
         <div className="Toprated">
+            
             <h2>
                 Explore Top Rated Parks
             </h2>
             <div className="Park-rating">
-                <Rating />
-                <Rating />
-                <Rating />
                 <Rating />
             </div>
         </div>
